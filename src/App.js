241dfa8e76f956b2f3fc1630774ff1455f50e0d3
raajeev.ranjann <raajeev.ranjann@gmail.com>
2022-12-11 23:46:58 +0530

    beggining

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; //importing react library
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Users from "./user/pages/Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          {/* <Route path="/:userId/places">
+            <UserPlaces exact />
+          </Route> */}
+          {/* <Route path="/places/new" exact>
+            <NewPlace />
+          </Route> */}
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/user/pages/Users.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Users.css";
+
+const Users = () => {
+  return <h2>I am User</h2>;
+};
+
+export default Users;
